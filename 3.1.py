b17bf4982799072289fce2325d70d28aa19dec8a
prerita1720 <morashiyaprerita@gmail.com>
2025-10-04 19:30:23 +0530

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,46 @@
+#1 answer
+
+oddeven =int(input("enter the value and check it is odd or even : "))
+print(oddeven)
+
+
+
+if():
+    print("the value is even")
+else:
+    print("the value is odd")
+
+#2 answer 
+age = int(input("enter the age :"))
+
+if age < 0:
+    print("Invalid age entered.")
+elif age <= 12:
+    print(" You are a Child.")
+elif 13 <= age <= 19:
+    print(" You are a Teenager.")
+elif 20 <= age <= 35:
+    print(" You are a Young Adult.")
+elif age >= 60:
+    print(" You are a Senior Citizen.")
+else:
+    print(" Age classification not found.")
+
+# 3 answer
+a=int(input("please enter value for a:"))
+b=int(input("please enter value for b:"))
+c=int(input("please enter value for c:"))
+
+
+if a>b:
+    print("a is greater than b and c")
+
+elif b>a:
+    print("b is greter than a and c")
+
+else:
+    print("a and b and c are same")    
+
+    print("Odd")
+
+# 4 answer
