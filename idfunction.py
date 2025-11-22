938c8f9613ffd74086e2b70c4a915e55af34dc55
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:07:20 +0530

    Add files via upload

---
+++ idfunction.py
@@ -0,0 +1,19 @@
+# integers
+a = 10
+b = 10
+c = 11
+d = 12
+
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+
+
+# class id of student 
+Prerita =100
+Dev =200
+
+print(id(Prerita))
+print(id(Dev))
+
