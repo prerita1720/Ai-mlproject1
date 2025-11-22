1495b99c51e871e06324b6c7ee7a86a98589cce8
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:08:33 +0530

    Add files via upload

---
+++ part2.py
@@ -0,0 +1,15 @@
+ysds #part 2
+
+data4 = "A \"meaningful statement\" is a declaration that conveys a significant idea, opinion, or message"
+
+# indexing
+print(data4[0])
+print(data4[1])
+print(data4[2])
+print(data4[-1])
+print(data4[-2])
+
+# slicing[start: stop : step]
+print(data4[0 : 13])
+print(data4[14 : ])
+print (data4[0 : : 3])
