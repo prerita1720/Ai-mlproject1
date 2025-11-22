02a03234f34ed8bba3b7e7f082dc22636ee2d59b
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:17:24 +0530

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,44 @@
+#==     equal           (a==b)is not true 
+# != not equal          (a!=b) is true.
+# >     greater than    (a>b) is not true
+#<  less than           (a<b)is true
+#>= greater than or equal to (a>=b)is not true
+#<= less than or equal to (a<=b)is true
+
+
+a=10
+b=11
+
+
+# == equal
+print("a == b:",a==b)
+print("true == 1: ",True == 1)
+print("false == 0: ",False == 0)
+
+# != not equal
+print("a!=b: ",a != b)
+
+a=10
+b=10
+# >  greater than
+print(a>b)
+print(b>a)
+
+
+
+# >  less than
+a=11
+b=10
+print("a < b:",a<b)
+print("b < a:",b<a)
+
+c=10
+print("c < c:",c<c)
+
+#>= greater han or equal to 
+a=10
+b=20
+
+print("a>b",a>=b)
+
+
