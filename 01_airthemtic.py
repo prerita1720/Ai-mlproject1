6a4cbc3d934fae327d972eca4f47ec33a88fbcbb
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:17:02 +0530

    Add files via upload

---
+++ 01_airthemtic.py
@@ -0,0 +1,58 @@
+#+  addition   a+b]]00000=30
+#-  subtraction a b=-10
+#*  multiplication a*b=200
+#/  division    b/a=2
+#% modulus  b% a=0
+#** Exponent    a**b =10**20
+#// floor division  9//2=4
+
+a=20
+b=0
+result =None
+
+
+# +
+result =a+b
+print(result)
+
+#-
+result =a-b
+print("a-b:",result)
+
+# *
+result =a*b
+print("a*b:",result)
+
+#/ output in always float value
+result = a/b
+print("a/b:",result)
+
+#**
+result=b**a
+print("b**a:",result)
+
+result=a**b
+print("a**b:",result)
+
+#// output in always in float value
+
+result = a//b
+print("a//b:",result)
+
+result = b//a
+print("b//a:",result)
+
+import math
+
+print(1234.2343)
+print(math.floor(1234.23453))
+print(math.ceil(1234.23453))
+print(round(1234.4343))
+
+# % modulus
+a = 8
+b = 2
+print("a % b :",a % b)
+
+#install the python 
+
