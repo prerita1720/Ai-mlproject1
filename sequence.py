04ee229d62c2d07fb91b657aad68cfd9c030d99b
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:11:04 +0530

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,48 @@
+#list
+
+s1 = "dev"
+s2 = "prerita"
+s3 = "gopi"
+s4 = "diya"
+
+students = ["dev","prerita" , "gopi" , "diya"]
+value = ["dev", 22 ,True, 99.99, [11 , 12 , "13" ,14, 13]]
+
+print(students)
+print(type(students))
+
+
+print(students[0], type(students[0]) , students[0] [1] )
+print(students[1])
+print(students[2])
+print(students[3])
+
+print(value)
+print(value[4])
+print(value[4][2])
+print(value[4][2][0])
+
+marks1 =[11, 12 ,13 , 14 , 15]
+marks2 =[21,22,23 ,24 ,25]
+
+print(marks1*3)
+
+
+print(marks1 + marks2)
+#hello dev
+print("hello " + "dev" )
+
+#empty list
+new_list = []
+old_list = ["old" ,"data"]
+
+
+
+
+      
+
+      
+
+
+
+
