fc411eb9809a26e82262b4171308cffec734886b
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:10:19 +0530

    Add files via upload

---
+++ numeric.py
@@ -0,0 +1,24 @@
+# 1. int (...-10, -9..........0,1,2,3,4.....)
+data1 = 10
+print(data1)
+
+
+#2. float
+
+data2 =1.0000215
+data3 = 1.00000007215
+print(data2 + data3)
+print(type(data3))
+
+#3.complex
+data4 = 10+20j
+print(data4)
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
