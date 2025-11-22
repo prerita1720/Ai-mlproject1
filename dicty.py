c2ec28b7e5f50211063b5dbb058a537b2b120474
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:13:15 +0530

    Add files via upload

---
+++ dicty.py
@@ -0,0 +1,27 @@
+#
+user1 = ["dev", 22, 9879868899 , "dev_207" ,"20-07-2003" ,"dev@207"]
+user2 = ["jaimin" ,"asdf@1234" , 25 , 7908292988 , "Mr.jemmy" ,"14-11-xxxx"]
+user1={
+    "name" : "dev",
+    "age" :  22, 
+    "phone_no" : 8460702944,
+    "DOB" : "20-7-2003",
+    "password" : "dev@207",
+    "username" : "dev_207",
+}
+
+user2 = {
+    "name" : "jaimin",
+    "age":24,
+    "phone_no" : 846689902,
+    "DOB" : "14-11-xxxx",
+    "password" :  "xyz",
+    "username"  : "mrjemmy",
+}
+
+print(user1)
+print(user1["name"])
+
+print(user2)
+print(user2["name"])
+print(user2["username"])
