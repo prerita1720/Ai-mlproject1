6824da078864fa5903c9fa51baaf31d895393e99
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:06:48 +0530

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,18 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+num1 = num2 = num3 = 2  
+  #initializing num1, num2, num3 to two
+
+print(num1, num2, num3)
+
+#cost of vegetable
+papya = 4
+orange =3
+cherry = 2
+print(papya,orange,cherry)
+
+
+
