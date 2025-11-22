58d7b7e2b9cbd2791c85e22df9b71369bee07da2
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:18:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-print("prerita")
-print("hello! i am prerita ")
-print("welcome to phython")
-print("here i am studying AI&ML data science")
+a=10
+b=20
+c=30
+d=40
+e=5
+#and
+print(a > b or a<b and c > d)
+
+#or
+
+print(a>b or c<d)
+
+#not
+
+print(not a>b and c<d)
+print(not(a>b and c<d))
+
+print((a+b)*e)
+print(a+b*e)
