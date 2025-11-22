090522d5acafa8c0a9002e8b63943bd965943aa0
prerita1720 <morashiyaprerita@gmail.com>
2025-11-22 21:19:24 +0530

    Add files via upload

---
+++ memembership.py
@@ -0,0 +1,16 @@
+message = 'A "meaningful statement" is a declaration that conveys a significant idea,'\
+    'opinion, or message'
+
+print(message)
+
+is_in = "that" in message  # is_in is a variable 
+
+print(is_in)
+
+#not in
+
+print(message)
+
+is_in = "that" not in message
+
+print(is_in)
